7bf2f2d9ba1d93623ab465254fd3748b00045bcc
Jovana Panic <panic.sv64.2022@uns.ac.rs>
2026-05-18 16:25:04 +0200

    Initial Spring project setup

---
+++ src/main/java/sbnz/szrn/SzrnApplication.java
@@ -0,0 +1,13 @@
+package sbnz.szrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzrnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzrnApplication.class, args);
+	}
+
+}
